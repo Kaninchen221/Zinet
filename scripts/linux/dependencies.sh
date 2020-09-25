23ba36e7faf1ab2cef867622636640498ce7b59f
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-25 19:13:10 +0200

    Fix dependencies

--- scripts/linux/dependencies.sh
+++ scripts/linux/dependencies.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 sudo apt-get install libx11-dev
-sudo apt-get install xorg-dev
-sudo apt-get install freeglut3-dev
-sudo apt-get install libudev-dev
 sudo apt-get install x11-utils
-sudo apt-get install udev
-sudo apt-get install oggfwd
+sudo apt-get install xorg-dev=
+sudo apt-get install libudev-dev
 sudo apt-get install libopenal-dev
 sudo apt-get install libvorbis-dev
 sudo apt-get install libvorbisfile3
