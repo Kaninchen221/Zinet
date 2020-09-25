27213fb8f35e4fe10036ee7e04b29a61f7e6d5c4
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-25 18:19:19 +0200

    Fix script

--- linux.sh
+++ linux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 chmod 777 scripts/linux/*.sh
 
-scripts/linux/install_cmake.sh
-scripts/linux/dependencies.sh
+./scripts/linux/install_cmake.sh
+./scripts/linux/dependencies.sh
 
 mkdir build
 cd build
