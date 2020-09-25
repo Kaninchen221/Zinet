a82040be28ffc1a4ce20e327deb7eb7f17ec4db4
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-25 20:42:55 +0200

    Fix dependencies

--- scripts/linux/dependencies.sh
+++ scripts/linux/dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install libx11-6 libx11-dev xorg-dev freeglut3-dev libudev-dev libopenal-data libopenal-dev libvorbis-dev
+sudo apt install libx11-6 libx11-dev xorg-dev freeglut3-dev libudev-dev libopenal-data libopenal-dev libvorbis-dev libflac-dev
