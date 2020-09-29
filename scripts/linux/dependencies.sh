330dd457403dca6d6bed4ac86a4683b3a344914a
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-29 17:17:18 +0200

    More dependencies for linux build

--- scripts/linux/dependencies.sh
+++ scripts/linux/dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install libx11-6 libx11-dev xorg-dev freeglut3-dev libudev-dev libopenal-data libopenal-dev libvorbis-dev libflac-dev
+sudo apt install libx11-6 libx11-dev xorg xorg-dev freeglut3 freeglut3-dev libudev1 libudev-dev libopenal-data libopenal-dev libvorbis0a libvorbis-dev libflac++6v5 libflac-dev
