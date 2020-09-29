1644221c1c20a4b90f601f595ecfbc7b10f6c194
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-29 20:31:15 +0200

    More whitespace

--- cmake/zinet_libs_include_directories.cmake
+++ cmake/zinet_libs_include_directories.cmake
@@ -1,4 +1,5 @@
 
 target_include_directories(Sandbox PRIVATE ${ZT_CORE_INCLUDE_DIR})
 target_include_directories(Sandbox PRIVATE ${ZT_SYSTEM_INCLUDE_DIR})
+
 target_include_directories(ZtSystem PRIVATE ${ZT_CORE_INCLUDE_DIR})
