e35e04793f26fcf21b66a485c8e2025ca2c3f0aa
Paweł Krolik <pawel8krolik@gmail.com>
2020-10-01 16:11:44 +0200

    More includes

--- cmake/zinet_libs_include_directories.cmake
+++ cmake/zinet_libs_include_directories.cmake
@@ -2,5 +2,8 @@
 target_include_directories(Sandbox PRIVATE ${ZT_CORE_INCLUDE_DIR})
 target_include_directories(Sandbox PRIVATE ${ZT_SYSTEM_INCLUDE_DIR})
 
+target_include_directories(ZtMain PRIVATE ${ZT_CORE_INCLUDE_DIR})
+target_include_directories(ZtMain PRIVATE ${SPDLOG_INCLUDE_DIRS})
+
 target_include_directories(ZtSystem PRIVATE ${ZT_CORE_INCLUDE_DIR})
 target_include_directories(ZtSystem PRIVATE ${ZT_MAIN_INCLUDE_DIR})
