725ee5f16369ceaa3bde71ea71c03e536171d513
Paweł Krolik <pawel8krolik@gmail.com>
2020-10-03 19:02:15 +0200

    Add tests for ZtLogger class

---
+++ tests/Main-tests/ZtLogger-tests.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "gtest/gtest.h"
+
+#include "Zinet/Main/ZtLogger.h"
+
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+class LoggerTests : public ::testing::Test {
+protected:
+
+    LoggerTests() {
+    }
+
+    ~LoggerTests() override {
+    }
+
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+
+};
+
+TEST_F(LoggerTests, CreateAndGetLogger) {
+
+    std::string loggerName = "CreateAndGetLogger logger";
+    std::string pattern = "*** [%H:%M:%S %z] [thread %t] %v ***";
+
+    zt::Logger::CreateLogger(loggerName, pattern);
+    auto savedLogger = zt::Logger::GetLogger(loggerName);
+
+    ASSERT_TRUE(savedLogger);
+    ASSERT_EQ(savedLogger->name(), loggerName);
+
+    zt::Logger::Info(loggerName, "Test Message");
+}
