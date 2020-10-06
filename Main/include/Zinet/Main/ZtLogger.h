2f8cc2dff47b251f2155cabdb22d51b313368671
Kaninchen221 <pawel8krolik@gmail.com>
2020-10-06 19:01:21 +0200

    Logger reimplementation

--- Main/include/Zinet/Main/ZtLogger.h
+++ Main/include/Zinet/Main/ZtLogger.h
@@ -10,6 +10,10 @@ namespace zt {
 
 	public:
 
+		struct Settings {
+			std::string pattern;
+		};
+
 		using LoggerT = std::shared_ptr<spdlog::logger>;
 
 		static void Trace(const std::string& loggerName, const std::string& message);
@@ -19,13 +23,43 @@ namespace zt {
 		static void Error(const std::string& loggerName, const std::string& message);
 		static void Critical(const std::string& loggerName, const std::string& message);
 
-		static LoggerT CreateLogger(const std::string& loggerName, const std::string& pattern);
-		static LoggerT CreateFileLogger(const std::string& loggerName, const std::string& pattern);
-
-		static LoggerT GetLogger(const std::string& loggerName);
+		static std::string GetLoggsFolder();
+		static std::string GetLoggsFileFormat();
 
 	private:
 
+		static LoggerT GetOrCreateLogger(const std::string& loggerName);
+
+		static LoggerT CreateDefaultLogger(const std::string& loggerName);
+
+		static LoggerT GetLogger(const std::string& loggerName);
+
+		static void AddStdoutSinkToLogger(LoggerT logger);
+		static void AddFileSinkToLogger(LoggerT logger);
+
+		template<class FuncT>
+		static void CatchAndPrintExceptionToStandardOutput(FuncT function, const std::string& functionInfoMacro) noexcept;
+
+		inline static std::string LoggsFolder = "logs/";
+		inline static std::string LoggsFileFormat = ".txt";
+		inline static bool Truncate = true;
 	};
+
+	template<class FuncT>
+	inline void Logger::CatchAndPrintExceptionToStandardOutput(FuncT function, const std::string& functionInfoMacro) noexcept
+	{
+		try {
+			std::invoke(function);
+		}
+		catch (const spdlog::spdlog_ex& spdlogEx) {
+			std::cout << "Function " + functionInfoMacro + " throw exception : " + spdlogEx.what() << '\n';
+		}
+		catch (const std::exception& ex) {
+			std::cout << "Function " + functionInfoMacro + " throw exception : " + ex.what() << '\n';
+		}
+		catch (...) {
+			std::cout << "Uncatchable exception type from function : " << functionInfoMacro << '\n';
+		}
+	}
 
 }
--- tests/Main-tests/ZtLogger-tests.h
+++ tests/Main-tests/ZtLogger-tests.h
@@ -6,6 +6,9 @@
 
 #include "spdlog/sinks/stdout_color_sinks.h"
 
+#include <filesystem>
+#include <fstream>
+
 class LoggerTests : public ::testing::Test {
 protected:
 
@@ -23,30 +26,10 @@ protected:
 
 };
 
-TEST_F(LoggerTests, CreateAndGetLogger) {
+TEST_F(LoggerTests, LoggingTest) {
 
     std::string loggerName = "CreateAndGetLogger logger";
-    std::string pattern = "*** [%H:%M:%S %z] [thread %t] %v ***";
+    std::string testMessage = "Test Message";
 
-    zt::Logger::CreateLogger(loggerName, pattern);
-    auto savedLogger = zt::Logger::GetLogger(loggerName);
-
-    ASSERT_TRUE(savedLogger);
-    ASSERT_EQ(savedLogger->name(), loggerName);
-
-    zt::Logger::Info(loggerName, "Test Message");
-}
-
-TEST_F(LoggerTests, CreateAndGetFileLogger) {
-
-    std::string loggerName = "CreateAndGetFileLogger logger";
-    std::string pattern = "*** [%H:%M:%S %z] [thread %t] %v ***";
-
-    zt::Logger::CreateFileLogger(loggerName, pattern);
-    auto savedLogger = zt::Logger::GetLogger(loggerName);
-
-    ASSERT_TRUE(savedLogger);
-    ASSERT_EQ(savedLogger->name(), loggerName);
-
-    zt::Logger::Info(loggerName, "Test Message");
+    zt::Logger::Info(loggerName, testMessage);
 }
