a1e13bc5287636f3386b2b4dde596599dbe78fed
Kaninchen221 <pawel8krolik@gmail.com>
2020-11-28 20:38:20 +0100

    Add Window and WindowSettings class

---
+++ Frame/include/Zinet/Frame/ZtWindow.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Zinet/Frame/ZtFrameConfig.h"
+
+namespace zt {
+
+	class ZINET_FRAME_API Window {
+
+
+
+	};
+
+}
---
+++ Frame/include/Zinet/Frame/ZtWindowSettings.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Zinet/Frame/ZtFrameConfig.h"
+
+namespace zt {
+
+	struct ZINET_FRAME_API WindowSettings {
+
+
+
+	};
+
+}
