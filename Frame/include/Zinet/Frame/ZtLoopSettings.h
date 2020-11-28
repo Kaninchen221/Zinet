7d489a8fdf10124adabaa260b8991a7f52404007
Kaninchen221 <pawel8krolik@gmail.com>
2020-11-28 17:49:02 +0100

    Add Loop Settings

---
+++ Frame/include/Zinet/Frame/ZtLoopSettings.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Zinet/Frame/ZtFrameConfig.h"
+
+#include <stdint.h>
+
+namespace zt {
+
+	struct ZINET_FRAME_API LoopSettings {
+
+		uint32_t maxFramesPerSecondCount;
+
+	};
+
+}
