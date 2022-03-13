f64286aee4d346ba7dd3d6b50a7c3fd67d4aab71
Kaninchen2121 <pawel8krolik@gmail.com>
2022-03-13 18:48:13 +0100

    Finish the files generator

--- PPG/__main__.py
+++ PPG/__main__.py
@@ -2,10 +2,4 @@ from PPG.root import *
 from PPG.generate_class import *
 
 root = generate_targets()
-
-print("---Sandbox---")
-
-file_generator = ZtFileGenerator()
-generate_result = file_generator.generate_class("ZtCore", "TestClass", True, root)
-print(generate_result)
-print(vars(file_generator))
+#create_class("ZtCore", "Test", root) #example
--- PPG/generate_class.py
+++ PPG/generate_class.py
@@ -1,5 +1,14 @@
 import pathlib
 import string
+from PPG.Utility import *
+
+
+def create_class(target, class_name, root):
+    file_generator = ZtFileGenerator()
+    generate_result = file_generator.generate_class(target, class_name, True, root)
+    if generate_result == False:
+        print("Failed to create class")
+
 
 class ZtFileGenerator():
 
@@ -13,7 +22,12 @@ class ZtFileGenerator():
 
     _target = None
 
-    # Target dirs
+    # Output paths
+    _header_output_path = pathlib.Path()
+    _source_output_path = pathlib.Path()
+    _test_output_path = pathlib.Path()
+
+    # Target paths
     _full_header_dir = pathlib.Path()
     _full_source_dir = pathlib.Path()
     _full_test_dir = pathlib.Path()
@@ -44,8 +58,13 @@ class ZtFileGenerator():
         self._source_template = ZtFileGenerator.get_template(ZtFileGenerator.source_template_path)
         self._test_template = ZtFileGenerator.get_template(ZtFileGenerator.test_template_path)
 
-        return True
+        self.prepare_output_paths(class_name)
 
+        self.generate_file(self._header_output_path, self._header_template)
+        self.generate_file(self._source_output_path, self._source_template)
+        self.generate_file(self._test_output_path, self._test_template)
+
+        return True
 
     def find_target(self, target_name, root):
         for target_lib in root.get_targets_libs():
@@ -93,3 +112,23 @@ class ZtFileGenerator():
     def get_template(template_path):
         file = open(template_path, mode='r')
         return file.read()
+
+    def prepare_output_paths(self, class_name):
+        self._header_output_path = self._full_header_dir / ("Zt" + class_name + ".h")
+        self._source_output_path = self._full_source_dir / ("Zt" + class_name + ".cpp")
+        self._test_output_path = self._full_test_dir / ("Zt" + class_name + "Tests.h")
+
+    def generate_file(self, output_path, template):
+        template = template.format_map(SafeDict(
+            argument_lib_main_header_path=self._lib_main_header_path,
+            argument_lib_namespace=self._lib_namespace,
+            argument_lib_api_macro=self._lib_api_macro,
+            argument_class_name=self._class_name,
+            argument_class_header_path=self._class_header_path,
+            argument_lib_test_namespace=self._lib_test_namespace,
+            argument_test_class_name=self._test_class_name,
+            argument_class_name_lower_case=self._class_name_lower_case
+        ))
+
+        file = open(output_path, "w+")
+        file.write(template)
