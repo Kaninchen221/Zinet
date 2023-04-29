227454a13e5bab486ebdfdeed9131ad1bb12e1a0
Kaninchen2121 <pawel8krolik@gmail.com>
2023-04-29 14:48:07 +0200

    Script return 1 if any test failed

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,7 +1,14 @@
 import os
+import subprocess
+import sys
 
 os.chdir("build/bin")
 
 for file in os.listdir():
     if file.endswith(".exe"):
-        os.system(file)
+        #os.system(file)
+        result = subprocess.run(file, capture_output=True)
+
+        if result.returncode != 0:
+            print(f"Process {file} return code {result.returncode}")
+            sys.exit(1)
