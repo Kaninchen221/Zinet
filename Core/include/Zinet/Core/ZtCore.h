f2a2fa66857245ff01fe160cb7384b9513d373e0
Kaninchen2121 <pawel8krolik@gmail.com>
2023-05-08 20:27:52 +0200

    Resolve TODO - Move Ensure to separated file

--- Core/include/Zinet/Core/ZtCore.h
+++ Core/include/Zinet/Core/ZtCore.h
@@ -33,26 +33,4 @@
 	#define ZINET_DEBUG
 #endif
 
-// Define code breakpoint
-// TODO Move it somewhere else
-#include <intrin.h>
-namespace zt
-{
-
-#ifdef ZINET_DEBUG
-
-	static inline bool Ensure(bool Value)
-	{
-		if (!Value)
-		{
-			__nop();
-			__debugbreak();
-		}
-
-		return Value;
-	}
-#else
-	static inline bool Ensure([[maybe_unused]] bool Value) { return true; }
-#endif
-
-}
+#include "Zinet/Core/ZtDebug.h"
