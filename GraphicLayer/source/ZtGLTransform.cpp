fb901fc627cc167c0b780d8f94e13d4aad59ef0b
Kaninchen2121 <pawel8krolik@gmail.com>
2023-05-20 18:16:11 +0200

    Change order from srt to trs

--- GraphicLayer/source/ZtGLTransform.cpp
+++ GraphicLayer/source/ZtGLTransform.cpp
@@ -11,11 +11,11 @@ namespace zt::gl
 	Matrix4f Transform::toMatrix() const
 	{
 		glm::mat4 result = glm::mat4(1.0f);
-		result = glm::scale(result, scale);
+		result = glm::translate(result, translation);
 		result = glm::rotate(result, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
 		result = glm::rotate(result, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
 		result = glm::rotate(result, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
-		result = glm::translate(result, translation);
+		result = glm::scale(result, scale);
 
 		return result;
 	}
