a502881e30740f678ce5ee4c063918270c3c46c8
Kaninchen2121 <pawel8krolik@gmail.com>
2023-05-27 16:09:39 +0200

    Reorder vertices
    Swap 2 and 3 vertex positons and texture coords

--- GraphicLayer/source/ZtGLSprite.cpp
+++ GraphicLayer/source/ZtGLSprite.cpp
@@ -78,14 +78,14 @@ namespace zt::gl
 		vertex.setTextureCoordinates({ 0.0f, 0.0f });
 		vertices.push_back(vertex);
 
-		vertex.setPosition({ 0.5f, 0.5f, 0.f });
+		vertex.setPosition({ -0.5f, 0.5f, 0.f });
 		vertex.setColor({ 0.0f, 0.0f, 1.0f, 1.0f });
-		vertex.setTextureCoordinates({ 0.0f, 1.0f });
+		vertex.setTextureCoordinates({ 1.0f, 1.0f });
 		vertices.push_back(vertex);
 
-		vertex.setPosition({ -0.5f, 0.5f, 0.f });
+		vertex.setPosition({ 0.5f, 0.5f, 0.f });
 		vertex.setColor({ 1.0f, 1.0f, 1.0f, 1.0f });
-		vertex.setTextureCoordinates({ 1.0f, 1.0f });
+		vertex.setTextureCoordinates({ 0.0f, 1.0f });
 		vertices.push_back(vertex);
 
 		BufferCreateInfo bufferCreateInfo{
@@ -101,7 +101,7 @@ namespace zt::gl
 
 	void Sprite::createIndexBuffer(RendererContext& rendererContext)
 	{
-		indices = { 0, 1, 2, 2, 3, 0 };
+		indices = { 0, 1, 3, 3, 2, 0 };
 		std::uint64_t size = sizeof(decltype(indices)::value_type) * indices.size();
 
 		BufferCreateInfo bufferCreateInfo{
