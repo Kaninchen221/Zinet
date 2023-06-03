7330209c3320f297d6b0328468c36e2394d880d8
Kaninchen2121 <pawel8krolik@gmail.com>
2023-06-03 19:50:08 +0200

    Update timeout

--- GraphicLayer/test_functional/source/ZtGLRendererTests.cpp
+++ GraphicLayer/test_functional/source/ZtGLRendererTests.cpp
@@ -263,7 +263,7 @@ namespace zt::gl::tests
 
 			renderer.postDraw();
 
-			if (clock.getElapsedTime().getAsSeconds() > 10000)
+			if (clock.getElapsedTime().getAsSeconds() > 10)
 			{
 				rendererContext.getWindow().requestCloseWindow();
 			}
