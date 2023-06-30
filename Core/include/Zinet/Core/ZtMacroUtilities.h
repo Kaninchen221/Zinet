3aa143b1e01a9682149c7bd21f958d7584c41698
Kaninchen221 <pawel8krolik@gmail.com>
2023-06-30 21:45:21 +0200

    Resolve TODO

--- Core/include/Zinet/Core/ZtMacroUtilities.h
+++ Core/include/Zinet/Core/ZtMacroUtilities.h
@@ -2,14 +2,6 @@
 
 #include "Zinet/Core/ZtCore.h"
 
-// TODO (medium) Refactor this file
-
-/// Breakpoint from code
-#define ZINET_BREAKPOINT __debugbreak 
-
-#define ZINET_ALWAYS_BREAK ZT_BREAKPOINT
-
-/// Macro utilities
 #define ZINET_CONCAT2(A, B) A ## B
 #define ZINET_CONCAT3(A, B, C) ZINET_CONCAT2(A, B) ## C
 #define ZINET_CONCAT4(A, B, C, D) ZINET_CONCAT3(A, B, C) ## D
