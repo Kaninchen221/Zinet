3519d0a9caeb59da18a0cff07c852b944a254a7b
Kaninchen221 <pawel8krolik@gmail.com>
2023-07-30 11:01:43 +0200

    ZIN-35
    Fix link errors

--- recipe_root.py
+++ recipe_root.py
@@ -9,5 +9,5 @@ root.projectVersion = "0.1.2"
 root.subdirectories = ["Core", "GraphicLayer", "EntryPoint"]
 root.globalCompileOptions = "/W4 /WX /external:W0 /external:anglebrackets /MP"
 root.globalCompileOptionsForAddressSanitizer = "/fsanitize=address /RTCu /experimental:module-"
-root.globalCompileDefinitions = "ZINET_STATIC ZINET_WINDOWS"
+root.globalCompileDefinitions = "ZINET_STATIC ZINET_WINDOWS _DISABLE_VECTOR_ANNOTATION _DISABLE_STRING_ANNOTATION"
 projectGenerator.add_generator(root)
