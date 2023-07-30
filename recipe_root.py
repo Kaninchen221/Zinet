5f26233914b674eb0b146dc1a89576a118b89839
Kaninchen221 <pawel8krolik@gmail.com>
2023-07-30 11:03:46 +0200

    NO_TASK
    Remove comment

--- recipe_root.py
+++ recipe_root.py
@@ -5,7 +5,6 @@ root.cmakeMinimumVersion = "3.20"
 root.projectDescription = "Game Engine"
 root.projectName = "Zinet"
 root.projectVersion = "0.1.2"
-#root.subdirectories = ["EntryPoint"]
 root.subdirectories = ["Core", "GraphicLayer", "EntryPoint"]
 root.globalCompileOptions = "/W4 /WX /external:W0 /external:anglebrackets /MP"
 root.globalCompileOptionsForAddressSanitizer = "/fsanitize=address /RTCu /experimental:module-"
