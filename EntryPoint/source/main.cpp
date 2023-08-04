3d4fcbe27a0c4d661b959af8f664fc453fb6e839
Kaninchen221 <pawel8krolik@gmail.com>
2023-08-04 23:36:00 +0200

    ZIN-35
    Turn on c++23

--- EntryPoint/source/main.cpp
+++ EntryPoint/source/main.cpp
@@ -1,6 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <format>
+//#include <print>
 
 int main()
 {
+    std::vector<int> ints { 2, 4, 6, 1, 3, 5 };
+    for (size_t index = 0u; auto element : ints)
+    {
+        //std::cout << "Index: " << index << " Value: " << element << '\n';
+        //printf("Index: %i Value: %i \n", (int)index, (int)element);
+        std::cout << std::format("Index: {} Value: {} \n", index, element);
+        ++index;
+    }
 
     return 0;
 }
