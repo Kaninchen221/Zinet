5869114a5dc97e2935d95cc73e7a2f60294a7112
Kaninchen221 <pawel8krolik@gmail.com>
2023-12-27 01:01:11 +0100

    ZIN-64
    - Code Injector

---
+++ PReflector/tests/test_code_injector.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from zinet_preflector.code_generators.code_generator_constructors import CodeGeneratorConstructors
+from zinet_preflector.code_generators.code_generator_getter_setter import CodeGeneratorGetterSetter
+from zinet_preflector.code_generators.code_generator_operators import CodeGeneratorOperators
+from zinet_preflector.code_injector import CodeInjector
+from zinet_preflector.parser import *
+from zinet_preflector.assignor import *
+from zinet_preflector.tokens_finder import *
+from zinet_preflector.code_generator import *
+
+
+class TestCodeInjector:
+
+    def test_inject_code(self):
+        path = str(Path(".").absolute() / "test_files/reflection_test_file.hpp")
+        parser = Parser()
+        parser_result = parser.parse(path)
+
+        assignor = Assignor()
+        assignor.sort(parser_result)
+
+        tokens_finder = TokensFinder()
+        tokens_finder.find_tokens(parser_result)
+
+        #print("Parse result after assigning:")
+        #print_parser_result(parser_result)
+
+        code_generator = CodeGenerator()
+        code_generator.instructions.append(CodeGeneratorConstructors())
+        code_generator.instructions.append(CodeGeneratorOperators())
+        code_generator.instructions.append(CodeGeneratorGetterSetter())
+
+        generated_code = code_generator.generate_code(parser_result)
+
+        #print("Generated code:")
+        #print(generated_code)
+        #print_generated_code(generated_code)
+
+        code_injector = CodeInjector()
+        code_injector.inject_code(path, generated_code)
+
+        file = open(path, 'r')
+        expected_file_path = str(Path(".").absolute() / "test_files/reflection_test_file_expected.hpp")
+        expected_file = open(expected_file_path, 'r')
+        file_content = file.read()
+        expected_file_content = expected_file.read()
+        assert file_content == expected_file_content
