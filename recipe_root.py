0ac8cf6bb864705942dce0b7bb9864a876e3344c
Kaninchen221 <pawel8krolik@gmail.com>
2024-01-19 23:53:03 +0100

    NO_TASK
    Fix compilation

--- recipe_root.py
+++ recipe_root.py
@@ -8,9 +8,8 @@ root.projectVersion = "0.1.2"
 root.subdirectories = ["CoreLib", "GraphicLayerLib", "EntryPoint", "EngineLib", "WindowLib", "MathLib", "ImGuiLib"]
 root.globalCompileOptions = "/W4 /WX /external:W0 /external:anglebrackets /MP"
 root.globalCompileOptionsForAddressSanitizer = "/fsanitize=address /RTCu /experimental:module-"
-root.globalCompileDefinitions = "ZINET_WINDOWS _DISABLE_VECTOR_ANNOTATION _DISABLE_STRING_ANNOTATION"
+root.globalCompileDefinitions = "ZINET_WINDOWS _DISABLE_VECTOR_ANNOTATION _DISABLE_STRING_ANNOTATION _SILENCE_STDEXT_ARR_ITERS_DEPRECATION_WARNING"
 project_generator.add_generator(root)
-
 from zinet_generator.cmakelists_generator_library import CMakeListsGeneratorLibrary
 
 CMakeListsGeneratorLibrary.libraryType = "SHARED"
